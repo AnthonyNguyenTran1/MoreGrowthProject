0e90efaeb91451d74012ea99c9f2c7eed29c6811
Kyan6868 <kyan6868@outlook.com>
2024-04-16 11:58:58 +1000

    syn

--- src/main/java/com/example/moregrowth/controller/EnquiryController.java
+++ src/main/java/com/example/moregrowth/controller/EnquiryController.java
@@ -1,76 +1,140 @@
 package com.example.moregrowth.controller;
 
-import com.example.moregrowth.mapper.EnquiryRepository;
-import com.example.moregrowth.model.Enquiry;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.moregrowth.model.Enquiry;
+import com.example.moregrowth.service.EnquiryService;
+
 @RestController
-@RequestMapping("/enquiries")
 public class EnquiryController {
 
     @Autowired
-    private EnquiryRepository enquiryRepository;
-
-    // get all 
-    @GetMapping
-    public List<Enquiry> getAllEnquiries() {
-        return enquiryRepository.findAll();
+    private EnquiryService enquiryService;
+    /*
+     *
+     * Search Functions
+     */
+    //view all the enquiries
+    @GetMapping("/all")
+    public List<Enquiry> getAll() {
+        return enquiryService.getAllEnquiries();
     }
-
-    // search base on ID
-    @GetMapping("/{id}")
-    public ResponseEntity<Enquiry> getEnquiryById(@PathVariable String id) {
-        Optional<Enquiry> enquiry = enquiryRepository.findById(id);
-        return enquiry.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    //search specific enquiry by using ID
+    @GetMapping("/search/id/{id}")
+    public Optional<Enquiry> getEnquiryById(@PathVariable String id) {
+        return enquiryService.findById(id);
     }
-
-    // post new enquiries
-    @PostMapping
-    public Enquiry createEnquiry(@RequestBody Enquiry newEnquiry) {
-        return enquiryRepository.insert(newEnquiry);
+    //search specific enquiry by using name
+    @GetMapping("/search/name/{name}")
+    public List<Enquiry> getEnquiryByName(@PathVariable String name) {
+        return enquiryService.findByName(name);
     }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Enquiry> updateEnquiry(@PathVariable String id, @RequestBody Enquiry updatedEnquiry) {
-        // Find whether the Inquiry object with the specified ID exists in the database
-        Optional<Enquiry> optionalEnquiry = enquiryRepository.findById(id);
-        
-        // Check whether the corresponding Inquiry object is found
-        if (optionalEnquiry.isPresent()) {
-            // get enquiry
-            Enquiry existingEnquiry = optionalEnquiry.get();
-            
-            // Update fields of an existing Inquiry object
-            existingEnquiry.setemailContent(updatedEnquiry.getemailContent());
-            existingEnquiry.setcontactMethod(updatedEnquiry.getcontactMethod());
-            existingEnquiry.setrole(updatedEnquiry.getrole());
-            existingEnquiry.settransactionResult(updatedEnquiry.gettransactionResult());
-            
-            
-            // save update info to database
-            Enquiry savedEnquiry = enquiryRepository.save(existingEnquiry);
-            
-            // return http and updated object
-            return ResponseEntity.ok(savedEnquiry);
-        } else {
-            // if cannot find, return warning
-            return ResponseEntity.notFound().build();
+    //search specific enquiry by using name
+    @GetMapping("/search/phoneNumber/{phoneNumber}")
+    public List<Enquiry> getEnquiryByPhoneNumber(@PathVariable String phoneNumber) {
+        return enquiryService.findByPhoneNumber(phoneNumber);
+    }
+    /*
+     *
+     *
+     * data provided functions
+     */
+    //provide total goodleads 
+    @GetMapping("/totalgoodleads/{leads}")
+    public long gettotalleads(@PathVariable String leads) {
+        return enquiryService.countByTransactionOutcome(leads);
+    }
+    //provide Open Enquiry in Good leads
+    @GetMapping("/Openleads")
+    public long getOpenleads() {
+        return  enquiryService.countByStatus("Open");
+    }
+    //provide Closed Enquiry in Good leads
+    @GetMapping("/Closedleads")
+    public long getClosedleads() {
+        return  enquiryService.countByStatus("Closed");
+    }
+    //provide Lost Enquiry in Good leads
+    @GetMapping("/Lostleads")
+    public long getLostleads() {
+        return  enquiryService.countByStatus("Lost");
+    }
+    //calc Conversion rate use closed/total
+    @GetMapping("/ConversionRate")
+    public String getConversionRate() {
+        Double df = enquiryService.countByStatus("Closed")*100.0/enquiryService.countByTransactionOutcome("Good");
+        DecimalFormat temp = new DecimalFormat("#.00");
+        String res = temp.format(df) + " %";
+        return res;
+    }
+    //get past 7 days good leads
+    @GetMapping("/past7goodleads")
+    public long[] getGoodleadseachday() {
+        LocalDate today = LocalDate.now();
+        long[] res = new long[7];
+        for(int i = 0; i < 7 ; i++){
+            LocalDate tempday = today.minusDays(i+1);
+            res[i] = enquiryService.countByDateAndTransactionOutcome(tempday);
         }
+        return res;
+    }
+    //get yesterday enquiries
+    @GetMapping("/past7enquiry")
+    public long[] getyesterdayenquiry() {
+        LocalDate today = LocalDate.now();
+        long[] res = new long[7];
+        for(int i =0;i < 7;i++){
+            LocalDate tempday = today.minusDays(i+1);
+            res[0] =enquiryService.countByDate(tempday);
+        }
+        return res;
+    }
+    //percentage of all the contact method
+    @GetMapping("/Contactpercentage/{method}")
+    public Double getContactpercentage(@PathVariable String method) {
+        return (double)enquiryService.countByContactMethod(method)/enquiryService.getTotalEnquiry();
+    }
+    /*
+     *
+     *
+     * List both Good and bad leads
+     */
+    @PostMapping("/listgood")
+    public List<Enquiry> listGood() {
+        return enquiryService.findbyTransactionOutcome("Good");
     }
 
-    // delete the enquiry
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteEnquiry(@PathVariable String id) {
-        if (enquiryRepository.existsById(id)) {
-            enquiryRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.notFound().build();
+    @PostMapping("/listbad")
+    public List<Enquiry> listbad() {
+        return enquiryService.findbyTransactionOutcome("Bad");
+    }
+    /*
+     *
+     *
+     * Management Functions
+     */
+    //update
+    @PutMapping("/enquiries/{id}")
+    public Enquiry updateEnquiry(@PathVariable String id, @RequestBody Enquiry enquiry) {
+        enquiry.setId(id);
+        return EnquiryService.save(enquiry);
+    }
+    
+    //delete specific enquiry
+    @DeleteMapping("delete/{id}")
+    public void deleteEnquiry(@PathVariable String id) {
+        EnquiryService.deleteById(id);
     }
 }
--- src/main/java/com/example/moregrowth/service/EnquiryService.java
+++ src/main/java/com/example/moregrowth/service/EnquiryService.java
@@ -1,4 +1,10 @@
 package com.example.moregrowth.service;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,13 +12,12 @@ import org.springframework.stereotype.Service;
 import com.example.moregrowth.mapper.EnquiryRepository;
 import com.example.moregrowth.model.Enquiry;
 
-import java.util.List;
-
+@Service
 public class EnquiryService {
-    
-    private final EnquiryRepository enquiryRepository;
 
     @Autowired
+    private static EnquiryRepository enquiryRepository;
+    
     public EnquiryService(EnquiryRepository enquiryRepository) {
         this.enquiryRepository = enquiryRepository;
     }
@@ -20,5 +25,74 @@ public class EnquiryService {
     public List<Enquiry> getAllEnquiries() {
         return enquiryRepository.findAll();
     }
+
+    public static void deleteById(String id) {
+        enquiryRepository.deleteById(id);
+        System.out.println("Delete Sucess");
+    }
+
+    public static Enquiry save(Enquiry enquiry) { 
+        if (enquiry.getId() != null && enquiryRepository.existsById(enquiry.getId())) {
+            // 如果Enquiry存在，更新它
+            return enquiryRepository.save(enquiry);
+        } else {
+            // 如果Enquiry不存在，可以选择抛出异常或保存新的Enquiry
+            throw new RuntimeException("Enquiry not found with id " + enquiry.getId());
+            // 或者可以选择创建一个新的Enquiry
+            // return enquiryRepository.save(new Enquiry(...));
+            }
+    }
+
+    public Optional<Enquiry> findById(String id) {
+        return enquiryRepository.findById(id);
+    }
+
+    public List<Enquiry> findByName(String name) {
+        return enquiryRepository.findByName(name);
+    }
+
+    public List<Enquiry> findByPhoneNumber(String phoneNumber) {
+        return enquiryRepository.findByPhoneNumber(phoneNumber);
+    }
+
+    public long countByTransactionOutcome(String leads) {
+        return  enquiryRepository.countByTransactionOutcome(leads);
+    }
+
+    public long countByStatus(String status) {
+        return enquiryRepository.countByStatus(status);
+    }
+
+    // 假设您的convertToLocalDateViaSqlDate方法如下定义：
+    public Date convertToLocalDateViaSqlDate(LocalDate localDate) {
+        ZoneId utcZoneId = ZoneId.of("UTC");
+        ZonedDateTime zdtStartOfDay = localDate.atStartOfDay(utcZoneId);
+        return Date.from(zdtStartOfDay.toInstant());
+    }
+
+    public long countByDateAndTransactionOutcome(LocalDate date) {
+        Date date1 = convertToLocalDateViaSqlDate(date);
+        return enquiryRepository.countByDateAndTransactionOutcome(date1, "Good");
+    }
+
+    public long countByDate(LocalDate yesterday) {
+        Date date1 = convertToLocalDateViaSqlDate(yesterday);
+        return enquiryRepository.countByDate(date1);
+    }
+
+    public long countByContactMethod(String method){
+        return enquiryRepository.countByContactMethod(method);
+    }
+
+    public long getTotalEnquiry() {
+        return enquiryRepository.count();
+    }
+
+    public List<Enquiry> findbyTransactionOutcome(String string) {
+        return enquiryRepository.findByTransactionOutcome(string);
+    }
+
+
+    
 
 }
