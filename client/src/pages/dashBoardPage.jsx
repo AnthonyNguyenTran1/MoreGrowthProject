6d0a5303667074c73df4548648e1e5b54f3530f4
AnthonyNguyenTran <anthonytran.nguyen1@gmail.com>
2024-05-01 00:49:06 +1000

    some progress

--- client/src/pages/dashBoardPage.jsx
+++ client/src/pages/dashBoardPage.jsx
@@ -1,7 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
 
 const DashBoardPage = () => {
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/all');
+            const data = await response.json();
+            setJsonData(data); // Store JSON data in state
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+
+}, []);
+  
+
   return (
-    <h1>Dashboard Page</h1>
+  
+    <div> 
+        <h1>Dashboard Page</h1>
+        {jsonData ? (
+            <p> {jsonData[0].text} </p> // Display JSON data
+        ) : (
+            <p>Loading data...</p> // Optional loading state
+        )}
+        {/* Add error state handling if needed */}
+
+    </div>
+
   )
 }
 
--- src/main/java/com/example/moregrowth/controller/EnquiryController.java
+++ src/main/java/com/example/moregrowth/controller/EnquiryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class EnquiryController {
      * Search Functions
      */
     //view all the enquiries
+    @CrossOrigin
     @GetMapping("/all")
     public List<Enquiry> getAll() {
         return enquiryService.getAllEnquiries();
