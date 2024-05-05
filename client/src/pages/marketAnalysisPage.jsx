a11c710b1c65186aa5d0c71174fe188e6eb55627
AnthonyNguyenTran <anthonytran.nguyen1@gmail.com>
2024-05-05 17:20:13 +1000

    in progress

--- client/src/pages/marketAnalysisPage.jsx
+++ client/src/pages/marketAnalysisPage.jsx
@@ -1,7 +1,38 @@
+import React, { useEffect, useState } from 'react';
 
 const MarketAnalysisPage = () => {
+
+  const [jsonData, setJsonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/all');
+            const data = await response.json();
+            setJsonData(data); // Store JSON data in state
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+
+  }, []);
+
+
   return (
-    <h1>Market Analysis Page</h1>
+    
+    <div> 
+        <h1>Market Analysis Page</h1>
+        {jsonData ? (
+            <p> {jsonData[0].text} </p> // Display JSON data
+        ) : (
+            <p>Loading data...</p> // Optional loading state
+        )}
+        {/* Add error state handling if needed */}
+
+    </div>
+
   )
 }
 
