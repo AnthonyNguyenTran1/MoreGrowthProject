4b6c8fb6e06ec9b653d2c756f3548514ae4d7ba0
Vito Jang <vitojibom@hotmail.com>
2024-05-21 16:53:27 +1000

    api connected to dashboard

--- client/src/pages/dashBoardPage.jsx
+++ client/src/pages/dashBoardPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Chart as ChartJS, defaults } from 'chart.js/auto'
 import { Bar, Doughnut, Line } from 'react-chartjs-2'
 import './dashBoardStyling.css'
@@ -12,11 +12,84 @@ defaults.plugins.title.color = "black";
 defaults.plugins.title.font.size = 20;
 
 const DashBoardPage = () => {
+  const [totalGoodLeads, setTotalGoodLeads] = useState(null);
+  const [totalBadLeads, setTotalBadLeads] = useState(null);
+  const [openLeads, setOpenLeads] = useState(null);
+  const [closedLeads, setClosedLeads] = useState(null);
+  const [lostLeads, setLostLeads] = useState(null);
+  const [conversionRate, setConversionRate] = useState(null);
+  const [phoneCallMethod, setPhoneCallMethod] = useState(null);
+  const [emailMethod, setEmailMethod] = useState(null);
+  const [socialMediaMethod, setSocialMediaMethod] = useState(null);
+  const [dailyEnquiries, setDailyEnquiries] = useState([]);
+
+  useEffect(() => {
+    // Asynchronous function to fetch data
+    const fetchData = async () => {
+      // Fetching data from API, parsing the response JSON data, and updating state variable 
+      try {
+        // TotalGoodLeads API
+        const responseTotalGoodLeads = await fetch("http://localhost:8080/totalgoodleads/Good");
+        const dataTotalGoodLeads = await responseTotalGoodLeads.json();
+        setTotalGoodLeads(dataTotalGoodLeads);
+
+        //TotalBadLeads API
+        const responseTotalBadLeads = await fetch("http://localhost:8080/totalgoodleads/Bad");
+        const dataTotalBadLeads = await responseTotalBadLeads.json();
+        setTotalBadLeads(dataTotalBadLeads);
+
+        // OpenLeads API 
+        const responseOpenLeads = await fetch("http://localhost:8080/Openleads");
+        const dataOpenLeads = await responseOpenLeads.json();
+        setOpenLeads(dataOpenLeads);
+
+        // ClosedLeads API
+        const responseClosedLeads = await fetch("http://localhost:8080/Closedleads");
+        const dataClosedLeads = await responseClosedLeads.json();
+        setClosedLeads(dataClosedLeads);
+
+        // LostLeads API
+        const responseLostLeads = await fetch("http://localhost:8080/Lostleads");
+        const dataLostLeads = await responseLostLeads.json();
+        setLostLeads(dataLostLeads)
+
+        // LeadConversion API
+        const responseConversionRate = await fetch("http://localhost:8080/ConversionRate");
+        const dataConversionRate = await responseConversionRate.text();
+        setConversionRate(dataConversionRate);
+
+        // MethodOfEnquiry API
+        const responseSocialMediaMethod = await fetch("http://localhost:8080/contactMethod/Social%20Media");
+        const dataSocialMediaMethod = await responseSocialMediaMethod.json();
+        setSocialMediaMethod(dataSocialMediaMethod);
+
+        const responseEmailMethod = await fetch("http://localhost:8080/contactMethod/Email");
+        const dataEmailMethod = await responseEmailMethod.json();
+        setEmailMethod(dataEmailMethod);
+
+        const responsePhoneCallMethod = await fetch("http://localhost:8080/contactMethod/Phone%20Call")
+        const dataPhoneCallMethod = await responsePhoneCallMethod.json();
+        setPhoneCallMethod(dataPhoneCallMethod);
+
+        // DailyEnquiries API
+        const responseDailyEnquiries = await fetch("http://localhost:8080/past7enquiry");
+        const dataDailyEnquiries = await responseDailyEnquiries.json();
+        setDailyEnquiries(dataDailyEnquiries)
+        console.log(dailyEnquiries)
+
+      } catch (err) {
+        // Log any errors that occur during the fetch process
+        console.log(err)
+      };
+    }
+    fetchData();
+  }, [])
+
   return (
     <div className='dashBoardStyle'>
       <div className='pageHeaderStyle'>
         <h1>Welcome, Vito</h1>
-        
+
       </div>
       <div className='dataCards'>
         <div className="dataCard totalLeads">
@@ -28,19 +101,19 @@ const DashBoardPage = () => {
               <p>Lost</p>
             </div>
             <div className='statusValue'>
-              <p>20</p>
-              <p>15</p>
-              <p>9</p>
+              <p>{openLeads}</p>
+              <p>{closedLeads}</p>
+              <p>{lostLeads}</p>
             </div>
             <div className='totalLeadsContainer'>
-              <p className='totalLeadsValueTop'>44</p>
+              <p className='totalLeadsValueTop'>{totalGoodLeads}</p>
               <p className='totalLeadsValueBottom'>No. of total leads</p>
             </div>
           </div>
         </div>
         <div className='dataCard conversionRate'>
           <p className='cardDataHeader'>Lead Conversion Rate</p>
-          <p className='conversionRateValue'>47%</p>
+          <p className='conversionRateValue'>{conversionRate}</p>
         </div>
       </div>
       <div className='dataCards'>
@@ -50,7 +123,7 @@ const DashBoardPage = () => {
               labels: ["Good Lead", "Bad Lead"],
               datasets: [{
                 label: "Enquiries",
-                data: [44, 25]
+                data: [totalGoodLeads, totalBadLeads]
               }]
             }}
             options={{
@@ -65,10 +138,10 @@ const DashBoardPage = () => {
         <div className='dataCard methOfEnq'>
           <Doughnut
             data={{
-              labels: ["Email", "Instagram", "Facebook"],
+              labels: ["Email", "Social Media", "Phone Call"],
               datasets: [{
                 label: "Count",
-                data: [53, 30, 45],
+                data: [emailMethod, socialMediaMethod, phoneCallMethod],
                 borderRadius: 5
               }]
             }}
--- client/src/pages/marketAnalysisPage.jsx
+++ client/src/pages/marketAnalysisPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Bar, Doughnut } from "react-chartjs-2";
+import { Bar, Doughnut, Pie } from "react-chartjs-2";
 import './marketAnalysisStyling.css';
 
 
@@ -14,43 +14,65 @@ const MarketAnalysisPage = () => {
   const [jsonDataCredit, setJsonDataCredit] = useState([]);
   const [jsonDataTimeframes, setJsonDataTimeframes] = useState(null);
 
+  const [phoneCallSourceEnquiry, setPhoneCallSourceEnquiry] = useState(null);
+  const [emailSourceEnquiry, setEmailSourceEnquiry] = useState(null);
+  const [instagramSourceEnquiry, setInstragramSourceEnquiry] = useState(null);
+  const [facebookSourceEnquiry, setFacebookSourceEnquiry] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/all');
-            const dataAll = await response.json();
+      try {
+        const response = await fetch('http://localhost:8080/all');
+        const dataAll = await response.json();
 
-            const responseIncomeLow = await fetch('http://localhost:8080/incomelevel/Low');
-            const dataIncomeLow = await responseIncomeLow.json();
+        const responseIncomeLow = await fetch('http://localhost:8080/incomelevel/Low');
+        const dataIncomeLow = await responseIncomeLow.json();
 
-            const responseIncomeMedium = await fetch('http://localhost:8080/incomelevel/Medium');
-            const dataIncomeMedium = await responseIncomeMedium.json();
+        const responseIncomeMedium = await fetch('http://localhost:8080/incomelevel/Medium');
+        const dataIncomeMedium = await responseIncomeMedium.json();
 
-            const responseIncomeHigh = await fetch('http://localhost:8080/incomelevel/High');
-            const dataIncomeHigh = await responseIncomeHigh.json();
+        const responseIncomeHigh = await fetch('http://localhost:8080/incomelevel/High');
+        const dataIncomeHigh = await responseIncomeHigh.json();
 
-            const responseIncomeVeryHigh = await fetch('http://localhost:8080/incomelevel/Very High');
-            const dataIncomeVeryHigh = await responseIncomeVeryHigh.json();
+        const responseIncomeVeryHigh = await fetch('http://localhost:8080/incomelevel/Very High');
+        const dataIncomeVeryHigh = await responseIncomeVeryHigh.json();
 
-            const responseCredit = await fetch('http://localhost:8080/classify');
-            const dataCredit = await responseCredit.json();
+        const responseCredit = await fetch('http://localhost:8080/classify');
+        const dataCredit = await responseCredit.json();
 
-            const responseDecision = await fetch('http://localhost:8080/TimeFrame');
-            const dataDecision = await responseDecision.json();
-            
-            setJsonData(dataAll); // Store JSON data in state
-            setJsonDataIncomeLow(dataIncomeLow);
-            setJsonDataIncomeMedium(dataIncomeMedium);
-            setJsonDataIncomeHigh(dataIncomeHigh);
-            setJsonDataIncomeVeryHigh(dataIncomeVeryHigh);
+        const responseDecision = await fetch('http://localhost:8080/TimeFrame');
+        const dataDecision = await responseDecision.json();
 
-            setJsonDataCredit(dataCredit);
-            setJsonDataTimeframes(dataDecision);
+        // SourceOfEnquiry API
+        const responsePhoneCallSourceEnquiry = await fetch("http://localhost:8080/inquirySource/Direct%20Call")
+        const dataPhoneCallSourceEnquiry = await responsePhoneCallSourceEnquiry.json();
+        setPhoneCallSourceEnquiry(dataPhoneCallSourceEnquiry);
+
+        const responseEmailSourceEnquiry = await fetch("http://localhost:8080/inquirySource/Property%20Portal")
+        const dataEmailSourceEnquiry = await responseEmailSourceEnquiry.json();
+        setEmailSourceEnquiry(dataEmailSourceEnquiry);
+
+        const responseFacebookSourceEnquiry = await fetch("http://localhost:8080/inquirySource/Facebook")
+        const dataFacebookSourceEnquiry = await responseFacebookSourceEnquiry.json();
+        setFacebookSourceEnquiry(dataFacebookSourceEnquiry);
+
+        const responseInstagramSourceEnquiry = await fetch("http://localhost:8080/inquirySource/Instagram%20Ad")
+        const dataInstragramSourceEnquiry = await responseInstagramSourceEnquiry.json();
+        setInstragramSourceEnquiry(dataInstragramSourceEnquiry);
+
+        setJsonData(dataAll); // Store JSON data in state
+        setJsonDataIncomeLow(dataIncomeLow);
+        setJsonDataIncomeMedium(dataIncomeMedium);
+        setJsonDataIncomeHigh(dataIncomeHigh);
+        setJsonDataIncomeVeryHigh(dataIncomeVeryHigh);
+
+        setJsonDataCredit(dataCredit);
+        setJsonDataTimeframes(dataDecision);
 
 
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
     fetchData();
@@ -65,38 +87,38 @@ const MarketAnalysisPage = () => {
 
 
   return (
-    
+
     <div className='dashBoardStyle charts-container'>
       <div className='pageHeaderStyle'>
         <h1>Market Analysis Page</h1>
       </div>
       <div className='dataCards'>
         <div className='creditRange methOfCredit barFormatted'>
-            <Doughnut
-                data={{
-                  labels: ["Low", "Medium", "High","Very High"],
-                  datasets: [
-                    {
-                      label: "Leads",
-                      data: [jsonDataIncomeLow, jsonDataIncomeMedium, jsonDataIncomeHigh,jsonDataIncomeVeryHigh]
-                    }
-                  ],
-                }}
-                options={{
-                  plugins: {
-                    title: {
-                      text: "Credit Score Classify"
-                    }
-                  }
-                }}
-                /> 
-        
+          <Doughnut
+            data={{
+              labels: ["Low", "Medium", "High", "Very High"],
+              datasets: [
+                {
+                  label: "Leads",
+                  data: [jsonDataIncomeLow, jsonDataIncomeMedium, jsonDataIncomeHigh, jsonDataIncomeVeryHigh]
+                }
+              ],
+            }}
+            options={{
+              plugins: {
+                title: {
+                  text: "Credit Score Classify"
+                }
+              }
+            }}
+          />
+
         </div>
 
         <div className='dataCards creditRange methOfCredit barFormatted'>
           <Bar
             data={{
-              labels: ["Excellent", "Very Good", "Average", "Fair","Low"],
+              labels: ["Excellent", "Very Good", "Average", "Fair", "Low"],
               datasets: [
                 {
                   label: "Leads",
@@ -111,9 +133,9 @@ const MarketAnalysisPage = () => {
                 }
               }
             }}
-            /> 
+          />
         </div>
-      
+
         <div className='methOfCredit creditRange barFormatted'>
           <h2> Decision TimeFrame Chart </h2>
           <Bar
@@ -126,9 +148,29 @@ const MarketAnalysisPage = () => {
                 }
               ],
             }}
-            />
+          />
         </div>
-        
+
+        <div className='dataCard'>
+          <Pie 
+            data={{
+              labels: ["Instagram Ads", "Facebook Ads", "Property Portal", "Direct Calls"],
+              datasets: [{
+                label: "Count",
+                data: [instagramSourceEnquiry, facebookSourceEnquiry, emailSourceEnquiry, phoneCallSourceEnquiry],
+                borderRadius: 2
+              }]
+            }}
+            options={{
+              plugins: {
+                title: {
+                  text: "Source of Enquiries"
+                }
+              }
+            }}
+          />
+        </div>
+
       </div>
     </div>
 
